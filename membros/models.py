0552e4139bd241df076de11c274887b1423ac7b2
robertomacedo <macedorss20@gmail.com>
2021-01-23 22:42:34 -0300

    alterado membros

--- membros/models.py
+++ membros/models.py
@@ -6,7 +6,6 @@ class CadMembro(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone_namber = models.CharField(max_length=30)
-    cpf = models.CharField(max_length=20, unique=True)
     conjuge = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
