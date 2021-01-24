479906456e00225cbaaf76dac2091060abbb2d6a
robertomacedo <macedorss20@gmail.com>
2021-01-23 22:47:25 -0300

    mudança de interpreter

--- membros/models.py
+++ membros/models.py
@@ -10,3 +10,5 @@ class CadMembro(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
